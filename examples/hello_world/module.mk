449e6ef2a82a71abfe916dab989a63933c1be9fd
carlos morales <carlosmorales@carloss-MBP.attlocal.net>
2022-08-08 22:00:04 -0700

    Adds support for multiple example binaries and keeps artifacts in build directories

--- examples/hello_world/module.mk
+++ examples/hello_world/module.mk
@@ -1,10 +1,9 @@
 local_app_name := hello_world
 local_src := $(wildcard $(subdirectory)/src/*.c)
-
 local_bin := $(subdirectory)/$(BINDIR)
+
 bindirs   += $(local_bin)
 sources   += $(local_src)
-examples  += $(local_bin)/hello_world.axf
-examples  += $(local_bin)/hello_world.bin
+examples  += $(local_bin)/$(local_app_name).axf
+examples  += $(local_bin)/$(local_app_name).bin
 mains     += $(local_bin)/$(local_app_name).o
-# $(eval $(call make-axf, $(local_src)), hello_world)

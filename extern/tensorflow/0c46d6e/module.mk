268630939d2a585794b5001ce63085f409d35111
Scott Leishman <scott.leishman@gmail.com>
2022-09-30 11:03:15 -0700

    AS4.3 fix, tf version names, mldebug fix.

--- extern/tensorflow/0c46d6e/module.mk
+++ extern/tensorflow/0c46d6e/module.mk
@@ -2,4 +2,9 @@ includes_api += $(subdirectory)/.
 includes_api += $(subdirectory)/third_party
 includes_api += $(subdirectory)/tensorflow/lite/micro/tools/make/downloads/flatbuffers/include
 
+ifeq ($(MLDEBUG),1)
+lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite-debug.a
+else
 lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite.a
+endif
+

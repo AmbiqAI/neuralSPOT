b1c96dab90d8fcad9f9c33691678894890405e1d
carlos morales <carlosmorales@carloss-MBP-2.attlocal.net>
2022-10-03 14:49:00 -0700

    Copies all header files when making a nest, removes for TF cruft, adds new TF static lib compiled with O2 for smaller size

--- extern/tensorflow/0c46d6e/module.mk
+++ extern/tensorflow/0c46d6e/module.mk
@@ -1,10 +1,11 @@
 includes_api += $(subdirectory)/.
 includes_api += $(subdirectory)/third_party
-includes_api += $(subdirectory)/tensorflow/lite/micro/tools/make/downloads/flatbuffers/include
+# includes_api += $(subdirectory)/tensorflow/lite/micro/tools/make/downloads/flatbuffers/include
+includes_api += $(subdirectory)/third_party/flatbuffers/include
 
 ifeq ($(MLDEBUG),1)
 lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite-debug.a
 else
-lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite.a
+lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite-o2.a
 endif
 
--- make/local_overrides.mk
+++ make/local_overrides.mk
@@ -1,7 +1,5 @@
 # Put your local overrides in here
 # EVB    :=blue_evb
 # DEFINES+= CFG_TUSB_DEBUG=2
-# TF_VERSION := b04cd98
+# TF_VERSION := R2.3.1
 AS_VERSION := R4.3.0
-
-

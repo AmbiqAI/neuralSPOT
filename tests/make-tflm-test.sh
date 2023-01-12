102fe16b7b7edf67e3fe127c6647e7fa1a9fdd4e
carlos morales <carlosmorales@carloss-MBP-2.attlocal.net>
2023-01-12 13:13:49 -0800

    Adds extended TFLM MicroProfiler support, fixes TFLM DebugLog support

--- tests/make-tflm-test.sh
+++ tests/make-tflm-test.sh
@@ -2,7 +2,7 @@
 set -e
 echo "Running all makes"
 for TF in "0c46d6e" "fecdd5d"; do
-    for FLAG in "MLDEBUG=1" "MLPROFILE=1" "MLPROFILE=1 ML_DEBUG=1"; do
+    for FLAG in "" "MLDEBUG=1" "MLPROFILE=1" "MLPROFILE=1 ML_DEBUG=1"; do
         make -f Makefile clean > /dev/null
         make -f Makefile TF_VERSION=${TF} ${FLAG} -j > /dev/null && echo "Make TF_VERSION=$TF $FLAG success" || echo "Make TF_VERSION=$TF $FLAG failed"
         test -f "build/examples/rpc_client_example/rpc_client.bin" || { echo "FAIL rpc client doesn't exit"; exit 1; }

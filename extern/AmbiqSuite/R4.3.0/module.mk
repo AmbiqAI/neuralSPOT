b0b1a70b468662af45aca8fdb16793eac0d3b27f
carlos morales <carlosmorales@carloss-MacBook-Pro-2.local>
2023-09-26 14:36:13 -0700

    Beta armclang support

--- extern/AmbiqSuite/R4.3.0/module.mk
+++ extern/AmbiqSuite/R4.3.0/module.mk
@@ -5,16 +5,19 @@ local_src += $(wildcard $(subdirectory)/src/*.s)
 
 # Base AmbiqSuite
 includes_api += $(subdirectory)/boards/$(BOARD)_$(EVB)/bsp
-# includes_api += $(subdirectory)/CMSIS/ARM/Include
+includes_api += $(subdirectory)/CMSIS/ARM/Include
 includes_api += $(subdirectory)/CMSIS/AmbiqMicro/Include
 includes_api += $(subdirectory)/devices
 includes_api += $(subdirectory)/mcu/$(BOARDROOT)
 includes_api += $(subdirectory)/mcu/$(BOARDROOT)/hal/mcu
 includes_api += $(subdirectory)/utils
 
+ifeq ($(TOOLCHAIN),arm)
+$(error neuralSPOT does not support armclang toolchain for this AmbiqSuite version)
+else
 lib_prebuilt += $(subdirectory)/lib/$(PART)/libam_hal.a
 lib_prebuilt += $(subdirectory)/lib/$(PART)/$(EVB)/libam_bsp.a
-# lib_prebuilt += $(subdirectory)/lib/libarm_cortexM4lf_math.a
+endif
 
 # Third-Party (FreeRTOS)
 includes_api += $(subdirectory)/third_party/FreeRTOSv10.1.1/Source/include

4fa574aad83dfe406c7c52f1ee2e12e7f9765964
carlos morales <carlosmorales@carloss-MacBook-Pro-2.local>
2023-10-04 09:24:02 -0700

    Revert "clean up some debug prints"
    This reverts commit 5fb492a963964161e10b2d041c437c1ea68bae6b.

--- tools/autodeploy/validator.py
+++ tools/autodeploy/validator.py
@@ -324,6 +324,7 @@ def validateModel(params, client, interpreter, md, mc):
                 buffer=input_data.flatten().tobytes(),
                 length=md.inputTensors[0].bytes,
             )
+        print("Calling EVB invoke pre ref")
         outputTensor = (
             erpc.Reference()
         )  # empty outputTensor, will be filled in by EVB RPC call
@@ -516,14 +517,14 @@ def compile_and_deploy(params, mc, first_time=False):
     d = params.working_directory + "/" + params.model_name
     d = d.replace("../", "")
     # Windows sucks
-    if os.name == "posix":
-        ws3 = "/dev/null"
-        ws = "-j"
-        ws1 = "&&"
+    if os.name == 'posix':
+        ws3 = '/dev/null'
+        ws = '-j'
+        ws1 = '&&'
     else:
-        ws3 = "NUL"
-        ws = "-j"
-        ws1 = "&"
+        ws3 = 'NUL'
+        ws = '-j'
+        ws1 = '&'
         # d = d.replace("/", "\\")
 
     if first_time:

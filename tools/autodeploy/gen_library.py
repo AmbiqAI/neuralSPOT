524ad41092ef3ba2e9502a269c889ce901a9adc8
carlos morales <carlosmorales@carloss-MacBook-Pro-2.local>
2023-10-04 09:24:02 -0700

    Revert "clean up some debug prints"
    This reverts commit af55a54dccd840703be489b3067f0ef0ca04d668.

--- tools/autodeploy/gen_library.py
+++ tools/autodeploy/gen_library.py
@@ -1,7 +1,6 @@
 import logging as log
 import os
 import shutil
-
 import numpy as np
 from ns_utils import createFromTemplate, xxd_c_dump
 
@@ -12,16 +11,16 @@ def generateModelLib(params, mc, md):
     adds, addsLen = mc.modelStructureDetails.getAddList()
     # arena_size = (arena_size // 1024) + 1
     # Windows sucks
-    if os.name == "posix":
-        ws_null = "/dev/null"
-        ws_j = "-j"
-        ws_and = "&&"
-        ws_p = "-p"
+    if os.name == 'posix':
+        ws_null = '/dev/null'
+        ws_j = '-j'
+        ws_and = '&&'
+        ws_p = '-p'
     else:
-        ws_null = "NUL"
-        ws_j = ""
-        ws_and = "&"
-        ws_p = ""
+        ws_null = 'NUL'
+        ws_j = ''
+        ws_and = '&'
+        ws_p = ''
 
     rm = {
         "NS_AD_NAME": n,
@@ -35,9 +34,7 @@ def generateModelLib(params, mc, md):
     print(f"Generating minimal library at {d}/{n}")
 
     # Generate a clean (no profiler) version of ns-model.a
-    os.system(
-        f"cd .. {ws_and} make clean >{ws_null} 2>&1 {ws_and} make {ws_j} >{ws_null} 2>&1"
-    )
+    os.system(f"cd .. {ws_and} make clean >{ws_null} 2>&1 {ws_and} make {ws_j} >{ws_null} 2>&1")
 
     # Make destination directory
     os.makedirs(f"{d}/{n}", exist_ok=True)
@@ -67,12 +64,14 @@ def generateModelLib(params, mc, md):
 
     # Copy needed files
     shutil.copy("../neuralspot/ns-core/src/gcc/startup_gcc.c", f"{d}/{n}/src/gcc/")
-    shutil.copy(
-        "../neuralspot/ns-core/src/armclang/startup_armclang.s",
-        f"{d}/{n}/src/armclang/",
-    )
+    shutil.copy("../neuralspot/ns-core/src/armclang/startup_armclang.s", f"{d}/{n}/src/armclang/")
+    # os.system(f"cp ../neuralspot/ns-core/src/startup_gcc.c {d}/{n}/src/")
+
     shutil.copy("autodeploy/templates/linker_script.ld", f"{d}/{n}/src/")
     shutil.copy("autodeploy/templates/ns_model.h", f"{d}/{n}/lib/")
+
+    # os.system(f"cp autodeploy/templates/linker_script.ld {d}/{n}/src/")
+    # os.system(f"cp autodeploy/templates/ns_model.h {d}/{n}/lib/")
 
     # Generate model weight file
     xxd_c_dump(

506ae9b153a148d74ff15f2675455aab2367de0e
carlos morales <carlosmorales@carloss-MacBook-Pro-2.local>
2023-10-25 12:07:47 -0700

    audio ble wip

---
+++ extern/codecs/opus-precomp/module.mk
@@ -0,0 +1,15 @@
+local_src := $(wildcard $(subdirectory)/src/*.c)
+local_src += $(wildcard $(subdirectory)/src/*.cc)
+local_src += $(wildcard $(subdirectory)/src/*.cpp)
+local_src += $(wildcard $(subdirectory)/src/*.s)
+includes_api += $(subdirectory)/includes-api
+
+ifeq ($(TOOLCHAIN),arm)
+lib_prebuilt += $(subdirectory)/libs/libopus.lib
+else
+lib_prebuilt += $(subdirectory)/libs/libopus.a
+endif
+
+local_bin := $(BINDIR)/$(subdirectory)
+bindirs   += $(local_bin)
+sources   += $(local_src)

419c460cc902aedc98ccdddbb4815ce0f78e278f
carlos morales <carlosmorales@carloss-MacBook-Pro-2.local>
2023-11-10 14:30:47 -0800

    Fix USB override linking problems in makefiles

--- neuralspot/ns-usb/module.mk
+++ neuralspot/ns-usb/module.mk
@@ -1,8 +1,19 @@
+# This is so stupid - make sure this happens first
+local_src := $(wildcard $(subdirectory)/src/overrides/*.c)
+local_src += $(wildcard $(subdirectory)/src/overrides/*.cc)
+local_src += $(wildcard $(subdirectory)/src/overrides/*.s)
+includes_api += $(subdirectory)/includes-api
+
+local_bin := $(BINDIR)/$(subdirectory)
+# bindirs   += $(local_bin)
+$(eval $(call make-override-library, $(local_bin)/ns-usb-overrides.a, $(local_src)))
+
+# Now the real lib
 local_src := $(wildcard $(subdirectory)/src/*.c)
 local_src += $(wildcard $(subdirectory)/src/*.cc)
 local_src += $(wildcard $(subdirectory)/src/*.s)
 includes_api += $(subdirectory)/includes-api
 
-local_bin := $(BINDIR)/$(subdirectory)
+# local_bin := $(BINDIR)/$(subdirectory)
 bindirs   += $(local_bin)
 $(eval $(call make-library, $(local_bin)/ns-usb.a, $(local_src)))

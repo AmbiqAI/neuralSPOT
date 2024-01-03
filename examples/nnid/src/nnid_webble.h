802a8381fb8f90c04b79e27dbec8d0417bad4148
carlos morales <carlosmorales@carloss-MacBook-Pro-2.local>
2024-01-03 11:00:03 -0800

    Enhances NNID to use web buttons instead of EVB

--- examples/nnid/src/nnid_webble.h
+++ examples/nnid/src/nnid_webble.h
@@ -43,8 +43,11 @@ ns_ble_service_t webbleService;          // Webble Service
 ns_ble_characteristic_t webbleOpusAudio; // Opus-encoded Audio Characteristic
 // ns_ble_characteristic_t bleIdLatency;           // How long NNID takes to run, per 10ms frame
 // ns_ble_characteristic_t bleOpusLatency;         // How long Opus takes to run, per 10ms frame
-ns_ble_characteristic_t webbleVad;     // State of VAD
-ns_ble_characteristic_t webbleMessage; // State of VAD
+ns_ble_characteristic_t webbleVad;           // State of VAD
+ns_ble_characteristic_t webbleMessage;       // Message to dashboard
+ns_ble_characteristic_t dashEnrollButton;    // Indicates a dashboard button was pressed
+ns_ble_characteristic_t dashIdButton;        // Indicates a dashboard button was pressed
+ns_ble_characteristic_t webbleNumUtterances; // Indicates a dashboard button was pressed
 
 // Values
 // extern unsigned char encodedDataBuffer[80];
@@ -60,6 +63,12 @@ int webbleNotifyHandler(ns_ble_service_t *s, struct ns_ble_characteristic *c) {
 int webbleReadHandler(ns_ble_service_t *s, struct ns_ble_characteristic *c, void *dest) {
     // ns_lp_printf("webbleReadHandler\n");
     memcpy(dest, c->applicationValue, c->valueLen);
+    return NS_STATUS_SUCCESS;
+}
+
+int webbleWriteHandler(ns_ble_service_t *s, struct ns_ble_characteristic *c, void *src) {
+    ns_lp_printf("webbleWriteHandler value %x\n", *(uint8_t *)src);
+    memcpy(c->applicationValue, src, c->valueLen);
     return NS_STATUS_SUCCESS;
 }
 
@@ -99,12 +108,29 @@ int audioWebbleServiceInit(void) {
         &webbleMessage, webbleUuid("5005"), &msgBuf, sizeof(msgBuf), NS_BLE_READ | NS_BLE_NOTIFY,
         NULL, NULL, &webbleNotifyHandler, 65000, true, &(webbleService.numAttributes));
 
+    ns_ble_create_characteristic(
+        &dashEnrollButton, webbleUuid("5006"), &enrollBtn, sizeof(enrollBtn),
+        NS_BLE_READ | NS_BLE_WRITE, &webbleReadHandler, &webbleWriteHandler, NULL, 0, false,
+        &(webbleService.numAttributes));
+
+    ns_ble_create_characteristic(
+        &dashIdButton, webbleUuid("5007"), &idBtn, sizeof(idBtn), NS_BLE_READ | NS_BLE_WRITE,
+        &webbleReadHandler, &webbleWriteHandler, NULL, 0, false, &(webbleService.numAttributes));
+
+    ns_ble_create_characteristic(
+        &webbleNumUtterances, webbleUuid("5008"), &numUtterances, sizeof(numUtterances),
+        NS_BLE_READ | NS_BLE_NOTIFY, NULL, NULL, &webbleNotifyHandler, 65000, true,
+        &(webbleService.numAttributes));
+
     // Create the service
-    webbleService.numCharacteristics = 3; // needed to allocate memory for characteristics
+    webbleService.numCharacteristics = 6; // needed to allocate memory for characteristics
     ns_ble_create_service(&webbleService);
     ns_ble_add_characteristic(&webbleService, &webbleOpusAudio);
     ns_ble_add_characteristic(&webbleService, &webbleVad);
     ns_ble_add_characteristic(&webbleService, &webbleMessage);
+    ns_ble_add_characteristic(&webbleService, &dashEnrollButton);
+    ns_ble_add_characteristic(&webbleService, &dashIdButton);
+    ns_ble_add_characteristic(&webbleService, &webbleNumUtterances);
     // ns_ble_add_characteristic(&webbleService, &bleSEEnabled);
     ns_ble_start_service(&webbleService); // Initialize BLE, create structs, start service
     return NS_STATUS_SUCCESS;

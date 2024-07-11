d8779e1ce7dda45c591ee5862fe33616e9763ba5
Evan <bugena123@gmail.com>
2024-07-11 13:54:17 -0700

    fixes usb unit test

--- neuralspot/ns-usb/tests/ns_usb_tests.c
+++ neuralspot/ns-usb/tests/ns_usb_tests.c
@@ -2,17 +2,18 @@
 #include "unity/unity.h"
 #include "ns_core.h"
 
-static uint8_t rx_buf[100];
-static uint8_t tx_buf[100];
-static ns_usb_config_t cfg;
+ uint8_t rx_buf[4096];
+ uint8_t tx_buf[4096];
+ ns_usb_config_t cfg;
+ usb_handle_t handle = NULL;
 
-static void reset_usb_cfg() {
+ void reset_usb_cfg() {
     cfg.api = &ns_usb_V1_0_0;
     cfg.deviceType = NS_USB_VENDOR_DEVICE;
-    cfg.rx_buffer = rx_buf;
-    cfg.rx_bufferLength = 10;
-    cfg.tx_buffer = tx_buf;
-    cfg.tx_bufferLength = 10;
+    cfg.rx_buffer = &rx_buf;
+    cfg.rx_bufferLength = 4096;
+    cfg.tx_buffer = &tx_buf;
+    cfg.tx_bufferLength = 4096;
     cfg.rx_cb = NULL;
     cfg.tx_cb = NULL;
     cfg.service_cb = NULL;
@@ -28,15 +29,13 @@ void ns_usb_tests_post_test_hook() {
 
 // Basic init should pass
 void ns_usb_test_init() {
-    usb_handle_t * handle;
-    uint32_t status = ns_usb_init(&cfg, handle);
+    uint32_t status = ns_usb_init(&cfg, &handle);
     TEST_ASSERT_EQUAL(NS_STATUS_SUCCESS, status);
 }
 
 // Null config should fail
 void ns_usb_test_init_null_config() {
-    usb_handle_t * handle;
-    uint32_t status = ns_usb_init(NULL, handle);
+    uint32_t status = ns_usb_init(NULL, &handle);
     TEST_ASSERT_EQUAL(NS_STATUS_INVALID_HANDLE, status);
 }
 
@@ -45,17 +44,17 @@ void ns_usb_test_init_null_buffers() {
     reset_usb_cfg();
     cfg.rx_buffer = NULL;
     cfg.tx_buffer = NULL;
-    usb_handle_t * handle;
-    uint32_t status = ns_usb_init(&cfg, handle);
+    uint32_t status = ns_usb_init(&cfg, &handle);
     TEST_ASSERT_EQUAL(NS_STATUS_INVALID_CONFIG, status);
 }
 
 // Invalid API
 void ns_usb_test_init_invalid_api() {
     reset_usb_cfg();
-    usb_handle_t * handle;
-    uint32_t status = ns_usb_init(&cfg, handle);
-    TEST_ASSERT_EQUAL(NS_STATUS_INVALID_HANDLE, status);
+    ns_core_api_t invalid_api = {.apiId = 0xFFFFF, .version = NS_USB_V0_0_1};
+    cfg.api = &invalid_api;
+    uint32_t status = ns_usb_init(&cfg, &handle);
+    TEST_ASSERT_EQUAL(NS_STATUS_INVALID_VERSION, status);
 }
 
 

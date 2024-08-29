ff6bc800a3f977d78affb3126f828d35738cf434
carlos morales <carlosmorales@carloss-MacBook-Pro-2.local>
2024-08-29 13:21:05 -0700

    Add edge impulse nest make targets, fixes exploding .bin problem for GCC13

--- projects/edgeimpulse/src/ambiq/debug_log.cpp
+++ projects/edgeimpulse/src/ambiq/debug_log.cpp
@@ -21,10 +21,11 @@
  */
 
 #include "../ei_classifier_porting.h"
-#include "ns_ambiqsuite_harness.h"
+
 
 #if EI_PORTING_AMBIQ == 1
 
+#include "ns_ambiqsuite_harness.h"
 #include "edge-impulse-sdk/tensorflow/lite/micro/debug_log.h"
 #include <stdio.h>
 #include <stdarg.h>
@@ -34,7 +35,7 @@
 extern "C"
 #endif // defined(__cplusplus) && EI_C_LINKAGE == 1
 void DebugLog(const char* s) {
-    ns_printf("%s", s);
+    ns_lp_printf("%s", s);
 }
 
 #endif // EI_PORTING_AMBIQ == 1

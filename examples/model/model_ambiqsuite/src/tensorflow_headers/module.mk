8340b2bd53b95aecd765ab1f1965748b905ce600
Evan <bugena123@gmail.com>
2024-10-23 10:41:00 -0700

    adds model, will delete later

---
+++ examples/model/model_ambiqsuite/src/tensorflow_headers/module.mk
@@ -0,0 +1,18 @@
+includes_api += $(subdirectory)/.
+includes_api += $(subdirectory)/third_party
+includes_api += $(subdirectory)/third_party/flatbuffers/include
+includes_api += $(subdirectory)/third_party/gemmlowp
+
+ifeq ($(TOOLCHAIN),arm)
+	ifeq ($(MLDEBUG),1)
+		lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite-cm4-armclang-debug.a
+	else
+		lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite-cm4-armclang-release.a
+	endif
+else
+	ifeq ($(MLDEBUG),1)
+		lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite-cm4-gcc-debug.a
+	else
+		lib_prebuilt += $(subdirectory)/lib/libtensorflow-microlite-cm4-gcc-release.a
+	endif
+endif

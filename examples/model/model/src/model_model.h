43b75fa7bac80e6bbd5d2f79fe12a255f6fb1cc8
Evan <bugena123@gmail.com>
2024-10-23 11:34:47 -0700

    deletes model created by autodeploy

--- examples/model/model/src/model_model.h
+++
@@ -1,6 +0,0 @@
-#define LOCAL_MODEL_NAME model
-// Calculated Arena size
-#define model_COMPUTED_ARENA_SIZE 23
-
-#define model_MODEL_LOCATION NS_AD_MODEL_LOCATION
-#define model_ARENA_LOCATION NS_AD_ARENA_LOCATION

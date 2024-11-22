901a91622a4cf43e364993303c3c18c6501b5104
Evan <bugena123@gmail.com>
2024-11-22 15:41:12 -0800

    ns_uart, wip. Receive data still broken

--- examples/uart/src/uart.cc
+++ examples/uart/src/uart.cc
@@ -6,62 +6,77 @@
 #include "ns_timer.h"
 #include "am_hal_mcuctrl.h"
 #include "ns_uart.h"
+#include <stdio.h>
+#include <string.h>
+#include "ns_peripherals_button.h"
 
 
-uint8_t g_pui8TxBuffer[256];
-uint8_t g_pui8RxBuffer[256];
+
+uint8_t *g_pui8TxBuffer;
+uint8_t *g_pui8RxBuffer;
 
 am_hal_uart_config_t g_sUartConfig =
 {
-    //
     // Standard UART settings: 115200-8-N-1
-    //
     .ui32BaudRate = 115200,
+#if defined(AM_PART_APOLLO3) || defined(AM_PART_APOLLO3P)
     .ui32DataBits = AM_HAL_UART_DATA_BITS_8,
+    // Set TX and RX FIFOs to interrupt at half-full.
     .ui32Parity = AM_HAL_UART_PARITY_NONE,
     .ui32StopBits = AM_HAL_UART_ONE_STOP_BIT,
     .ui32FlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
-
-    //
-    // Set TX and RX FIFOs to interrupt at half-full.
-    //
     .ui32FifoLevels = (AM_HAL_UART_TX_FIFO_1_2 |
                        AM_HAL_UART_RX_FIFO_1_2),
-
-    //
     // Buffers
-    //
     .pui8TxBuffer = g_pui8TxBuffer,
-    .ui32TxBufferSize = sizeof(g_pui8TxBuffer),
+    .ui32TxBufferSize = (uint32_t) sizeof(g_pui8TxBuffer),
     .pui8RxBuffer = g_pui8RxBuffer,
-    .ui32RxBufferSize = sizeof(g_pui8RxBuffer),
+    .ui32RxBufferSize = (uint32_t) sizeof(g_pui8RxBuffer),
+#endif
+#if defined(AM_PART_APOLLO4) || defined(AM_PART_APOLLO4P) || defined(AM_PART_APOLLO4L)
+    .eDataBits = AM_HAL_UART_DATA_BITS_8,
+    .eParity = AM_HAL_UART_PARITY_NONE,
+    .eStopBits = AM_HAL_UART_ONE_STOP_BIT,
+    .eFlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
+    .eTXFifoLevel = AM_HAL_UART_FIFO_LEVEL_16,
+    .eRXFifoLevel = AM_HAL_UART_FIFO_LEVEL_16,
+#endif
 };
 
 int main(void) {
     ns_core_config_t ns_core_cfg = {.api = &ns_core_V1_0_0};
-    ns_core_init(&ns_core_cfg);
+    NS_TRY(ns_core_init(&ns_core_cfg), "Core init failed.\n");
     uint32_t opusBegin, opusEnd;
+#if defined(AM_PART_APOLLO3) || defined(AM_PART_APOLLO3P)
     am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0); // nada
+#endif
+    NS_TRY(ns_power_config(&ns_development_default), "Power Init Failed.\n");
+    NS_TRY(ns_set_performance_mode(NS_MINIMUM_PERF), "Set CPU Perf mode failed.");
     ns_itm_printf_enable();                                     // nada
-    ns_power_config(&ns_development_default);
-    // am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
-    // am_hal_cachectrl_enable();
-    // NS_TRY(ns_set_performance_mode(NS_MINIMUM_PERF), "Set CPU Perf mode failed. ");
+    ns_interrupt_master_enable();
 
-    // am_hal_mcuctrl_control(AM_HAL_MCUCTRL_CONTROL_SRAM_PREFETCH);
-    am_hal_sysctrl_fpu_enable();              // nada
-    am_hal_sysctrl_fpu_stacking_enable(true); // nada
     ns_uart_config_t uart_config = {
         .api=&ns_uart_V0_0_1,
-        .uart_config = &g_sUartConfig
+        .uart_config = &g_sUartConfig,
     };
     ns_lp_printf("UART init\n");
     NS_TRY(ns_uart_init(&uart_config), "UART init failed.\n");
-
+    char buffer[] = "hello world";
     while (1) {
-        ns_uart_write("Hello World!\n");
-        ns_uart_read(&uart_config);
-        ns_lp_printf("%s\n", g_pui8RxBuffer);
-        ns_delay_us(1000000);
+        // Send data over UART
+        ns_lp_printf("Sent: hello world\n");
+        // Small delay to allow MCU to process and respond
+        ns_uart_send_data(&uart_config, buffer, (uint32_t)sizeof(buffer));
+        ns_delay_us(500000);
+        // Receive data over UART
+        ns_uart_receive_data(&uart_config);
+        // if (rx_length > 0) {
+            // rx_buffer[rx_length] = '\0'; // Null-terminate the received data
+        ns_lp_printf("Received: %s\n", g_pui8RxBuffer);
+        strcpy(buffer, (char *)g_pui8RxBuffer);
+        // } else {
+        //     ns_lp_printf("No data received.\n");
+        // }
+        ns_delay_us(500000);
     }
 }
--- neuralspot/ns-uart/src/apollo4/ns_uart.c
+++ neuralspot/ns-uart/src/apollo4/ns_uart.c
@@ -1,90 +1,103 @@
 #include "am_mcu_apollo.h"
 #include "am_bsp.h"
 #include "am_util.h"
+// #include "am_hal_global.h"
 #include "ns_uart.h"
-
 volatile uint32_t ui32LastError;
-
+bool g_bUARTdone = false;
+extern uint8_t g_pui8RxBuffer[256];
+extern uint8_t g_pui8TxBuffer[256];
 // UART0 interrupt handler.
 void
 am_uart_isr(void)
 {
-    // Service the FIFOs as necessary, and clear the interrupts.
-    uint32_t ui32Status, ui32Idle;
-    am_hal_uart_interrupt_status_get(phUART, &ui32Status, true);
-    am_hal_uart_interrupt_clear(phUART, ui32Status);
-    am_hal_uart_interrupt_service(phUART, ui32Status, &ui32Idle);
+    // // Service the FIFOs as necessary, and clear the interrupts.
+    // uint32_t ui32Status;
+    // am_hal_uart_interrupt_status_get(phUART, &ui32Status, true);
+    // am_hal_uart_interrupt_clear(phUART, ui32Status);
+    // am_hal_uart_interrupt_service(phUART, ui32Status);
+    am_hal_uart_interrupt_queue_service(phUART);
+
+}
+
+
+// non blocking callback function
+void uart_done(uint32_t ui32ErrorStatus, void *pvContext)
+{
+    g_bUARTdone = true;
 }
 
 uint32_t init_uart(am_hal_uart_config_t *uart_config)
 {
-    am_util_id_t sIdDevice;
-    uint32_t ui32StrBuf;
-
-    //
-    // Set the clock frequency.
-    //
-    am_hal_clkgen_control(AM_HAL_CLKGEN_CONTROL_SYSCLK_MAX, 0);
-
-    //
-    // Set the default cache configuration
-    //
-    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
-    am_hal_cachectrl_enable();
-
-    //
-    // Configure the board for low power operation.
-    //
-    am_bsp_low_power_init();
-
-    //
-    // Initialize the printf interface for UART output.
-    //
-    NS_TRY(am_hal_uart_initialize(0, &phUART), "Failed to initialize the UART");
-
-    NS_TRY(am_hal_uart_power_control(phUART, AM_HAL_SYSCTRL_WAKE, false), "Failed to power the UART");
-
-    am_hal_uart_clock_speed_e eUartClockSpeed = eUART_CLK_SPEED_DEFAULT ;
-    NS_TRY(am_hal_uart_control(phUART, AM_HAL_UART_CONTROL_CLKSEL, &eUartClockSpeed),  "Failed to set the UART clock speed");
-    NS_TRY(am_hal_uart_configure(phUART, uart_config),  "Failed to configure the UART");
 
     //
     // Enable the UART pins.
     //
     am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_TX, g_AM_BSP_GPIO_COM_UART_TX);
     am_hal_gpio_pinconfig(AM_BSP_GPIO_COM_UART_RX, g_AM_BSP_GPIO_COM_UART_RX);
+    NS_TRY(am_hal_uart_initialize(0, &phUART), "Failed to initialize the UART");
+    NS_TRY(am_hal_uart_power_control(phUART, AM_HAL_SYSCTRL_WAKE, false), "Failed to power the UART");
+    NS_TRY(am_hal_uart_configure(phUART, uart_config),  "Failed to configure the UART");
+    NS_TRY(am_hal_uart_buffer_configure(phUART, g_pui8TxBuffer, sizeof(g_pui8TxBuffer), g_pui8RxBuffer, sizeof(g_pui8RxBuffer)), "Failed to configure the UART buffers");
 
-    //
+    // Set the default cache configuration
+    // am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+    // am_hal_cachectrl_enable();
+
     // Enable interrupts.
-    //
+    NVIC_SetPriority((IRQn_Type)(UART0_IRQn + AM_BSP_UART_PRINT_INST), AM_IRQ_PRIORITY_DEFAULT);
     NVIC_EnableIRQ((IRQn_Type)(UART0_IRQn + AM_BSP_UART_PRINT_INST));
     am_hal_interrupt_master_enable();
 
-    //
-    // Set the main print interface to use the UART print function we defined.
-    //
-    am_util_stdio_printf_init(ns_uart_write);
     return AM_HAL_STATUS_SUCCESS;
 }
 
+void ns_uart_send_data(ns_uart_config_t* cfg, char * pcStr, uint32_t size) {
+    uint32_t ui32StrLen = 0;
+    uint32_t ui32BytesWritten = 0;
 
-
-void uart_write(char *pcStr, uint32_t ui32StrLen, uint32_t ui32BytesWritten) {
-    // Print the string via the UART.
+    // // Measure the length of the string.
+    // while(pcStr[ui32StrLen] != 0) {
+    //     ui32StrLen++;
+    // }
     const am_hal_uart_transfer_t sUartWrite =
     {
-        .ui32Direction = AM_HAL_UART_WRITE,
-        .pui8Data = (uint8_t *) pcStr,
-        .ui32NumBytes = ui32StrLen,
-        .ui32TimeoutMs = 0,
+        .eType = AM_HAL_UART_NONBLOCKING_WRITE,
+        .pui8Data = (uint8_t *)pcStr,
+        .ui32NumBytes = size,
         .pui32BytesTransferred = &ui32BytesWritten,
-    };
-
+        .ui32TimeoutMs = 100, // block for 5 seconds
+        .pfnCallback = &uart_done,
+        .pvContext = NULL,
+        .ui32ErrorStatus = 0
+    };    
+    g_bUARTdone = false;
     NS_TRY(am_hal_uart_transfer(phUART, &sUartWrite), "Failed to write to UART\n");
-
+    while (!g_bUARTdone);
     if (ui32BytesWritten != ui32StrLen)
     {
         // Couldn't send the whole string!!
         while(1);
     }
+    // am_hal_uart_append_tx(phUART, (uint8_t *)g_pui8TxBuffer, (uint32_t)sizeof(g_pui8TxBuffer));
+
+}
+
+void ns_uart_receive_data(ns_uart_config_t *cfg) {
+    // uint32_t ui32StrLen = 0;
+    // uint32_t ui32BytesRead = 0;
+
+    // const am_hal_uart_transfer_t sUartRead =
+    // {
+    //     .eType = AM_HAL_UART_NONBLOCKING_READ, // uses blocking read
+    //     .pui8Data = (uint8_t *) g_pui8RxBuffer,
+    //     .ui32NumBytes = ui32StrLen,
+    //     .pui32BytesTransferred = &ui32BytesRead,
+    //     .ui32TimeoutMs = 100, // 100 ms blocking
+    //     .pfnCallback = &uart_done,
+    //     .pvContext = NULL,
+    //     .ui32ErrorStatus = &ui32LastError
+    // };
+    // NS_TRY(am_hal_uart_transfer(phUART, &sUartRead), "Failed to read from UART\n");
+    am_hal_uart_get_rx_data(phUART, g_pui8RxBuffer, 14);
 }

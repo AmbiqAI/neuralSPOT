9758b8a95c4c87b38848407aa3cd068736b78c48
Evan <bugena123@gmail.com>
2024-12-04 11:08:00 -0800

    deletes commented out code

--- neuralspot/ns-uart/includes-api/ns_uart.h
+++ neuralspot/ns-uart/includes-api/ns_uart.h
@@ -27,25 +27,6 @@ extern "C" {
         { .major = 0, .minor = 0, .revision = 1 }
     #define NS_UART_API_ID 0xCA000B  
     #define UART_ID                            (0)
-    #define TIMER_NUM       0               ///< Timer number used in the example
-    #define MAX_UART_PACKET_SIZE            (2048)
-    #define UART_RX_TIMEOUT_MS              (5)
-    #define NUM_UARTS_USED 1
-    // #define NS_UART_OLDEST_SUPPORTED_VERSION NS_UART_V0_0_1
-    // #define NS_UART_CURRENT_VERSION NS_UART_V0_0_1
-    // #define TEST_UART_TX_MODULE     3
-    // #define TEST_UART_RX_MODULE     0
-
-    // #define CUST_UART0RX_PIN        62 // VERIFY ALL APOLLO PINS
-    // #define CUST_UART0RTS_PIN       58 // VERIFY ALL APOLLO PINS
-
-    // #define UART_DMA_ENABLED        1
-    // #define UART_BUFFER_SIZE        2048
-    // #define UART_QUEUE_SIZE         2048
-    // #define UART_TEST_PACKET_SIZE   2048
-
-// AM_SHARED_RW uint8_t ui8TxBuffer[UART_BUFFER_SIZE] __attribute__((aligned(32)));
-// AM_SHARED_RW uint8_t ui8RxBuffer[UART_BUFFER_SIZE] __attribute__((aligned(32)));
 
 extern const ns_core_api_t ns_uart_V0_0_1;
 extern const ns_core_api_t ns_uart_oldest_supported_version;
@@ -53,11 +34,6 @@ extern const ns_core_api_t ns_uart_current_version;
 
 typedef void * ns_uart_handle_t;
 extern ns_uart_handle_t phUART;
-
-// typedef void (*ns_uart_rx_cb)(ns_uart_transaction_t *);
-// typedef void (*ns_uart_tx_cb)(ns_uart_transaction_t *);
-// typedef void (*ns_uart_service_cb)(uint8_t);
-
 
 /**
  * @brief UART Configuration Struct used to configure hardware settings
@@ -71,7 +47,7 @@ typedef struct
 ns_uart_config_t;
 
 /**
- * @brief Initialize the USB system
+ * @brief Initialize the UART system
  *
  * @param cfg
  * @param handle

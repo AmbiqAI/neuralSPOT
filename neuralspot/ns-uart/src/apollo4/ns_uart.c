cad52183c7fa4a7f1ba3abd347b38ff876ce528c
Evan <bugena123@gmail.com>
2024-12-20 10:03:07 -0800

    hides some ambiqsuite code in example to ns-uart, adds retries for send data

--- neuralspot/ns-uart/src/apollo4/ns_uart.c
+++ neuralspot/ns-uart/src/apollo4/ns_uart.c
@@ -4,11 +4,23 @@
 #include "ns_uart.h"
 volatile uint32_t ui32LastError;
 bool g_bUARTdone = false;
-
+#define MAX_UART_RETRIES 10 // Maximum number of retries
 #define RX_BUFFER_SIZE 256
 #define TX_BUFFER_SIZE 256
 AM_SHARED_RW uint8_t g_pui8TxBuffer[TX_BUFFER_SIZE]__attribute__((aligned(32)));
 AM_SHARED_RW uint8_t g_pui8RxBuffer[RX_BUFFER_SIZE]__attribute__((aligned(32)));
+
+am_hal_uart_config_t g_sUartConfig =
+{
+    // Standard UART settings: 115200-8-N-1
+    .ui32BaudRate = 115200,
+    .eDataBits = AM_HAL_UART_DATA_BITS_8,
+    .eParity = AM_HAL_UART_PARITY_NONE,
+    .eStopBits = AM_HAL_UART_ONE_STOP_BIT,
+    .eFlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
+    .eTXFifoLevel = AM_HAL_UART_FIFO_LEVEL_16,
+    .eRXFifoLevel = AM_HAL_UART_FIFO_LEVEL_16,
+};
 
 void am_uart_isr(void)
 {
@@ -29,12 +41,12 @@ void uart_done(uint32_t ui32ErrorStatus, void *pvContext)
 
 uint32_t init_uart(am_hal_uart_config_t *uart_config)
 {
-    // Set the default cache configuration
-    am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
-    am_hal_cachectrl_enable();
+    // // Set the default cache configuration
+    // am_hal_cachectrl_config(&am_hal_cachectrl_defaults);
+    // am_hal_cachectrl_enable();
 
-    // Configure the board for low power operation.
-    am_bsp_low_power_init();
+    // // Configure the board for low power operation.
+    // am_bsp_low_power_init();
 
 #if defined(AM_PART_APOLLO4L)
     NS_TRY(am_hal_uart_initialize(2, &phUART), "Failed to initialize the UART");
@@ -56,48 +68,55 @@ uint32_t init_uart(am_hal_uart_config_t *uart_config)
 
 void ns_uart_send_data(ns_uart_config_t* cfg, char * txBuffer, uint32_t size) {
     uint32_t ui32BytesWritten = 0;
-
-    const am_hal_uart_transfer_t sUartWrite =
-    {
-        .eType = AM_HAL_UART_BLOCKING_WRITE,
-        .pui8Data = (uint8_t *)txBuffer,
-        .ui32NumBytes = size,
-        .pui32BytesTransferred = &ui32BytesWritten,
-        .ui32TimeoutMs = 1000, 
-        .pfnCallback = &uart_done,
-        .pvContext = NULL,
-        .ui32ErrorStatus = 0
-    };    
-    g_bUARTdone = false;
-    NS_TRY(am_hal_uart_transfer(phUART, &sUartWrite), "Failed to write to UART\n");
-    while (!g_bUARTdone);
-    if (ui32BytesWritten != size)
-    {
-        // Couldn't send the whole string!!
-        while(1);
+    uint32_t retries = 0;
+    uint32_t status = AM_HAL_STATUS_SUCCESS;
+    while(retries < MAX_UART_RETRIES) {
+        const am_hal_uart_transfer_t sUartWrite =
+        {
+            .eType = AM_HAL_UART_BLOCKING_WRITE,
+            .pui8Data = (uint8_t *)txBuffer,
+            .ui32NumBytes = size,
+            .pui32BytesTransferred = &ui32BytesWritten,
+            .ui32TimeoutMs = 1000, 
+            .pfnCallback = &uart_done,
+            .pvContext = NULL,
+            .ui32ErrorStatus = 0
+        };    
+        // g_bUARTdone = false;
+        status = am_hal_uart_transfer(phUART, &sUartWrite);
+        if (status == AM_HAL_STATUS_SUCCESS && ui32BytesWritten == size) {
+            // Successfully sent the whole string
+            am_hal_uart_tx_flush(phUART);
+            return AM_HAL_STATUS_SUCCESS;
+        }
+        retries++;
     }
-    am_hal_uart_tx_flush(phUART);
-}
+    return status;
+    }
 
 void ns_uart_receive_data(ns_uart_config_t *cfg, char * rxBuffer, uint32_t size) {
+    uint32_t retries = 0;
+    uint32_t status = AM_HAL_STATUS_SUCCESS;
     uint32_t ui32BytesRead = 0;
-
-    const am_hal_uart_transfer_t sUartRead =
-    {
-        .eType = AM_HAL_UART_BLOCKING_READ, // uses blocking read
-        .pui8Data = (uint8_t *)rxBuffer,
-        .ui32NumBytes = size,
-        .pui32BytesTransferred = &ui32BytesRead,
-        .ui32TimeoutMs = 1000,
-        .pfnCallback = &uart_done,
-        .pvContext = NULL,
-        .ui32ErrorStatus = &ui32LastError
-    };
-    NS_TRY(am_hal_uart_transfer(phUART, &sUartRead), "Failed to read from UART\n");
-    while (!g_bUARTdone);
-    if (ui32BytesRead != size)
-    {
-        // Couldn't send the whole string!!
-        while(1);
+    while (retries < MAX_UART_RETRIES) {
+        memset(g_pui8RxBuffer, 0, sizeof(g_pui8RxBuffer));
+        const am_hal_uart_transfer_t sUartRead =
+        {
+            .eType = AM_HAL_UART_BLOCKING_READ, // uses blocking read
+            .pui8Data = (uint8_t *)rxBuffer,
+            .ui32NumBytes = size,
+            .pui32BytesTransferred = &ui32BytesRead,
+            .ui32TimeoutMs = 1000,
+            .pfnCallback = &uart_done,
+            .pvContext = NULL,
+            .ui32ErrorStatus = &ui32LastError
+        };
+        status = am_hal_uart_transfer(phUART, &sUartRead);
+        if (status == AM_HAL_STATUS_SUCCESS) {
+            // Successfully read the whole string
+            return AM_HAL_STATUS_SUCCESS;
+        }
+        retries++;
     }
+    return status;
 }

96dfe79e59ea08bb84fd0df758d5d010910c8666
Evan <bugena123@gmail.com>
2025-01-02 14:44:30 -0800

    cleans up commented code and deletes unfinished erpc modifications

--- neuralspot/ns-uart/src/ns_uart.c
+++ neuralspot/ns-uart/src/ns_uart.c
@@ -26,35 +26,6 @@ const ns_core_api_t ns_uart_oldest_supported_version = {
 
 const ns_core_api_t ns_uart_current_version = {.apiId = NS_UART_API_ID, .version = NS_UART_V0_0_1};
 
-// // UART interrupt handler.
-// am_hal_uart_config_t g_sUartConfig =
-// {
-//     // Standard UART settings: 115200-8-N-1
-//     .ui32BaudRate = 115200,
-// #if defined(AM_PART_APOLLO3) || defined(AM_PART_APOLLO3P)
-//     .ui32DataBits = AM_HAL_UART_DATA_BITS_8,
-//     // Set TX and RX FIFOs to interrupt at half-full.
-//     .ui32Parity = AM_HAL_UART_PARITY_NONE,
-//     .ui32StopBits = AM_HAL_UART_ONE_STOP_BIT,
-//     .ui32FlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
-//     .ui32FifoLevels = (AM_HAL_UART_TX_FIFO_1_2 |
-//                        AM_HAL_UART_RX_FIFO_1_2),
-//     // Buffers
-//     .pui8TxBuffer = g_pui8TxBuffer,
-//     .ui32TxBufferSize = (uint32_t) sizeof(g_pui8TxBuffer),
-//     .pui8RxBuffer = g_pui8RxBuffer,
-//     .ui32RxBufferSize = (uint32_t) sizeof(g_pui8RxBuffer),
-// #endif
-// #if defined(AM_PART_APOLLO4) || defined(AM_PART_APOLLO4P) || defined(AM_PART_APOLLO4L)
-//     .eDataBits = AM_HAL_UART_DATA_BITS_8,
-//     .eParity = AM_HAL_UART_PARITY_NONE,
-//     .eStopBits = AM_HAL_UART_ONE_STOP_BIT,
-//     .eFlowControl = AM_HAL_UART_FLOW_CTRL_NONE,
-//     .eTXFifoLevel = AM_HAL_UART_FIFO_LEVEL_16,
-//     .eRXFifoLevel = AM_HAL_UART_FIFO_LEVEL_16,
-// #endif
-// };
-
 ns_uart_config_t ns_uart_config = {
     .api = &ns_uart_V0_0_1,
     .uart_config = NULL};

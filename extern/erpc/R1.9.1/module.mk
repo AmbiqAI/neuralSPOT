660d183483ad8bd4b94d9601080e0ab0367999cd
Evan <bugena123@gmail.com>
2025-01-23 09:45:01 -0800

    filters out erpc_usb_cdc_transport.hpp unless USB_PRESET == 1

--- extern/erpc/R1.9.1/module.mk
+++ extern/erpc/R1.9.1/module.mk
@@ -4,11 +4,13 @@ local_src := $(filter-out $(subdirectory)/src/erpc_usb_cdc_transport.cpp, $(wild
 local_src := $(filter-out $(subdirectory)/src/erpc_setup_usb_cdc.cpp, $(local_src))
 local_src += $(wildcard $(subdirectory)/src/*.s)
 includes_api += $(subdirectory)/includes-api
+includes_api := $(filter-out $(subdirectory)/includes-api/erpc_usb_cdc_transport.hpp, $(includes_api))
 
 # Conditionally include USB source files
-ifdef USB_PRESENT
+ifeq ($(USB_PRESENT), 1)
 local_src += $(subdirectory)/src/erpc_usb_cdc_transport.cpp
 local_src += $(subdirectory)/src/erpc_setup_usb_cdc.cpp
+includes_api := $(subdirectory)/includes-api/erpc_usb_cdc_transport.hpp, $(includes_api)
 endif
 local_bin := $(BINDIR)/$(subdirectory)
 bindirs   += $(local_bin)

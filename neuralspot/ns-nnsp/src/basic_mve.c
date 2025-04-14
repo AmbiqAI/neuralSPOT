894a7e24927145ec39983f195346d79a0053c905
poyupaulchen <poyupaulchen@gmail.com>
2025-04-14 16:50:04 -0700

    fix armclang hanging on nnsp

--- neuralspot/ns-nnsp/src/basic_mve.c
+++ neuralspot/ns-nnsp/src/basic_mve.c
@@ -3,6 +3,7 @@ Basic MVE Function for Helium architecture
 */
 #include <stdint.h>
 #include "ambiq_nnsp_debug.h"
+#include "third_party/ns_cmsis_nn/Include/arm_nnsupportfunctions.h"
 #if ARM_OPTIMIZED == 3
 #include "basic_mve.h"
 #include <arm_mve.h>
@@ -40,20 +41,10 @@ void move_data_16b(
         int16_t *dst,
         int len)
 {
-    int i;
-    int num = len >> 3;
-    int rem = len & 0x7;
-    int16x8_t *pt_src = (int16x8_t*) src;
-    int16x8_t *pt_dst = (int16x8_t*) dst;
-
-    for (i = 0; i < num; i++)
-        *pt_dst++ = *pt_src++;
-
-    if (rem) {
-        mve_pred16_t mask = vctp16q((uint32_t) rem);
-        int16x8_t tmp = vldrhq_z_s16((int16_t*) pt_src, mask);
-        vst1q_p_s16((int16_t*) pt_dst, tmp, mask);
-    }
+    arm_memcpy_s8(
+        (int8_t*) dst,
+        (int8_t*) src,
+        (len << 1));
 }
 
 void set_zero_32b(int32_t *dst, int len)
